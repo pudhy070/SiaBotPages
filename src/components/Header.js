03a3f111d96bf77eb62bd0e94857ddbfacce8ae8
pudhy070 <pudhy070@gmail.com>
2025-11-17 11:21:07 +0900

    수정 내용에 대한 설명

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,9 @@ const Header = () => {
     return (
         <header className="header">
             <div className="header-content">
-                <h1>Sia</h1>
+                <h1>김도훈</h1>
                 <p className="subtitle">Full Stack & AI Service Developer</p>
-                <div className="contact">p****@gmail.com</div>
+                <div className="contact">pudhy070@gmail.com</div>
             </div>
         </header>
     );
